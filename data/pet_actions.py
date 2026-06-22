93e18c70e012b096b125e8b7b033ad4c453b291b
zhaohanqing2026 <2388293958@qq.com>
2026-06-22 22:41:32 +0800

    Update pet_actions.py

--- data/pet_actions.py
+++ data/pet_actions.py
@@ -1,6 +1,10 @@
+# 宠物动作配置 组员B负责
 PET_ACTIONS = {
-    "walk": "随机漫步",
-    "idle": "待机动画（眨眼、摇尾巴）",
-    "click": "点击反应动画",
-    "drag": "鼠标拖拽移动"
+    "idle": "pet_img/idle.gif",
+    "running-right": "pet_img/running-right.gif",
+    "running-left": "pet_img/running-left.gif",
+    "click": "pet_img/click.gif",
+    "jumping": "pet_img/jumping.gif",
+    "failed": "pet_img/failed.gif",
+    "review": "pet_img/review.gif"
 }
